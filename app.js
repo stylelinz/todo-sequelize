f4125e42c6e178a71a224e794250f737c24c26e5
StyleLinz_Man <cwchuang1997@gmail.com>
2021-06-09 11:27:29 +0800

    feat: add routes & views of users #20210609001

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-import express from 'express'
-import exphbs from 'express-handlebars'
-import session from 'express-session'
-import methodOverride from 'method-override'
-import bcrypt from 'bcryptjs'
+const express = require('express')
+const exphbs = require('express-handlebars')
+const session = require('express-session')
+const methodOverride = require('method-override')
+const bcrypt = require('bcryptjs')
+
+const routes = require('./routes')
 
 const app = express()
 const PORT = 3000
@@ -14,10 +16,7 @@ app.engine('hbs', exphbs({
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-
-app.get('/', (req, res) => {
-  res.send('hello world.')
-})
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`Express is listening at http://localhost:${PORT}`)
